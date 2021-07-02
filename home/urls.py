45e7be9259d690263e3edcbe6aeaba24330e2a0a
AmodhShenoy <amodhshenoy@gmail.com>
2021-07-02 21:32:40 +0530

    Login module added

--- home/urls.py
+++ home/urls.py
@@ -3,9 +3,9 @@ from home import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.homePageView, name='homePage'),
-    # path('login/', views.loginView, name='login'),
-    # path('logout/', views.logoutView, name='logout'),
+    path('', views.indexView, name='index'),
+    path('login/', views.loginView, name='login'),
+    path('logout/', views.logoutView, name='logout'),
     # path('delete/', views.deleteView, name='delete'),
     # path('edit/', views.editView, name='edit'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from home.forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from pwdmanager.decorators import login_required
 
-# Create your views here.
-def homePageView(request):
+@login_required
+def indexView(request):
     return render(request, 'homePage.html')
+
+def loginView(request):
+    context = {}
+
+    if request.method == "GET":
+        form = LoginForm()
+        context['form'] = form
+    else:
+        form = LoginForm(data=request.POST)
+        context['form'] = form
+        if form.is_valid():
+            user = authenticate(
+                request, 
+                username=form.cleaned_data['username'], 
+                password=form.cleaned_data['password']
+            )
+            login(request, user)
+            return redirect('home:index')
+        else:
+            messages.add_message(
+                request,
+                messages.ERROR,
+                'Invalid username or password'
+            )    
+
+    return render(request, 'login.html', context)
+
+def logoutView(request):
+    logout(request)
+    return redirect('home:index')
